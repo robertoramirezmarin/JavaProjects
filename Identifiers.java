84d12e24835f783650676078a26f455ca0aa1174
Roberto Ramirez Marin <robertoramirezmarin.2003@gmail.com>
2022-12-05 20:01:56 -0600

    Second File

---
+++ Identifiers.java
@@ -0,0 +1,9 @@
+public class Identifiers {
+    public static void main (String[] pps){
+        double pi = 3.1416;
+        int radio = 15; 
+        double area = pi * (radio * radio); 
+        String message = "Resultado = "; 
+        System.out.println(message+area);
+    }
+}
